d0c3790dc14db97a8db906faed6dbfd80db4cb3c
Robert Salmon <robertsalmon88@gmail.com>
2022-08-24 11:06:54 -0400

    third commit

--- about.js
+++ about.js
@@ -1,11 +1,19 @@
 console.log("hello world");
 
 
+let imgTag = document.querySelector('image')
+
+const compliments = ['If your reading this you are a badass!']
+
+function handleMouseover(){
+	const randomIndex = Math.floor(Math.random * compliments.length-1)
+	let randomCompliment = compliment
+}
 
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('form was submitted successfully');
 }
 
 
---
+++ profile.js
@@ -0,0 +1,16 @@
+const favColor=black
+
+function myFavoriteColor(black) {
+    return(
+        myFavoriteColor=black
+        console.log(favColor)
+    )
+}
+
+
+
+
+
+addEventListener(myFavoriteColor,'Black')
+addEventListener(myFavoritePlace,'Panama City Beach')
+addEventListener(myFavoriteRitual,'something awesome')
